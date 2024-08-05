fd6a7a8eb37049f47c6e0f94730cf2900dce24a6
Patrick José Pereira <patrickelectric@gmail.com>
2024-08-05 10:28:28 -0700

    sim: Add simple simulator
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ sim/simple/scripts/TagsManager.gd
@@ -0,0 +1,28 @@
+extends Node3D
+
+func _ready():
+	var path = "res://tags/"
+	var i = 0
+	for file in DirAccess.get_files_at(path):
+		if not file.ends_with(".png"):
+			continue
+
+		# Get aruco id from image name
+		var regex = RegEx.new()
+		regex.compile("aruco_(?<id>[0-9]+)")
+		var result = regex.search(file)
+		if not result:
+			return
+		var aruco_id = result.get_string("id")
+
+		var sprite = Sprite3D.new()
+		sprite.texture = load(path + file)
+		var size = sprite.texture.get_width()
+		var size_m = 0.2
+		sprite.pixel_size = 0.2 / size
+		sprite.position = Vector3(1*i, 0, 0)
+		sprite.set_name("aruco_%d" % i)
+		sprite.set_meta("id", int(aruco_id))
+		sprite.set_meta("size_m", size_m)
+		i += 1
+		add_child(sprite)
