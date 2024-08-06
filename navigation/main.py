de4094998afd3671f597e55b3ff279f3af993a28
Joao Mario Lago <joao.mario.lago@hotmail.com>
2024-08-06 11:29:29 -0700

    Change to not require vehicle

--- navigation/main.py
+++ navigation/main.py
@@ -5,20 +5,22 @@ from world import World
 from slam import SLAM
 from vehicle import Vehicle
 
+
 def main() -> None:
     args = CommandLineArgs.from_args()
 
     camera = VideoCamera(args.camera)
     world = World(args.world)
     detector = SLAM(world, camera)
-    vehicle = Vehicle(args.vehicle)
+
+    if args.vehicle:
+        vehicle = Vehicle(args.vehicle)
 
     while True:
-        if not vehicle.is_vehicle_armed():
-            vehicle.force_arm()
-
         detector.detect()
-        vehicle.actuate(camera.position, camera.angles.yaw)
+
+        if args.vehicle:
+            vehicle.actuate(camera.position, camera.angles.yaw)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
