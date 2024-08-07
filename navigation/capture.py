95fde97f6d9fe40e74dece4f36335e0d74ef97b8
Joao Mario Lago <joao.mario.lago@hotmail.com>
2024-08-06 17:55:17 -0700

    Fix problems relatedwith gst init

--- navigation/capture.py
+++ navigation/capture.py
@@ -5,79 +5,37 @@ BlueRov video capture class
 
 import cv2
 import gi
+import os
 import numpy as np
 
 gi.require_version('Gst', '1.0')
-from gi.repository import Gst
+from gi.repository import Gst, GLib
 
 
 class Video():
-    """BlueRov video capture class constructor
 
-    Attributes:
-        port (int): Video UDP port
-        video_codec (string): Source h264 parser
-        video_decode (string): Transform YUV (12bits) to BGR (24bits)
-        video_pipe (object): GStreamer top-level pipeline
-        video_sink (object): Gstreamer sink element
-        video_sink_conf (string): Sink configuration
-        video_source (string): Udp source ip and port
-        latest_frame (np.ndarray): Latest retrieved video frame
-    """
-
-    def __init__(self, port=5600):
+    def __init__(self, port=5601):
         """Summary
 
         Args:
             port (int, optional): UDP port
         """
 
+        os.environ['GST_DEBUG'] = '3'
+
         Gst.init(None)
 
         self.port = port
         self.latest_frame = self._new_frame = None
 
-        # [Software component diagram](https://www.ardusub.com/software/components.html)
-        # UDP video stream (:5600)
-        self.video_source = 'udpsrc port={}'.format(self.port)
-        # [Rasp raw image](http://picamera.readthedocs.io/en/release-0.7/recipes2.html#raw-image-capture-yuv-format)
-        # Cam -> CSI-2 -> H264 Raw (YUV 4-4-4 (12bits) I420)
-        self.video_codec = '! application/x-rtp, payload=96 ! rtph264depay ! h264parse ! avdec_h264'
-        # Python don't have nibble, convert YUV nibbles (4-4-4) to OpenCV standard BGR bytes (8-8-8)
-        self.video_decode = \
-            '! decodebin ! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert'
-        # Create a sink to get data
-        self.video_sink_conf = \
-            '! appsink emit-signals=true sync=false max-buffers=2 drop=true'
-
         self.video_pipe = None
         self.video_sink = None
 
+        self.loop = GLib.MainLoop()
         self.run()
 
-    def start_gst(self, config=None):
-        """ Start gstreamer pipeline and sink
-        Pipeline description list e.g:
-            [
-                'videotestsrc ! decodebin', \
-                '! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert',
-                '! appsink'
-            ]
-
-        Args:
-            config (list, optional): Gstreamer pileline description list
-        """
-
-        if not config:
-            config = \
-                [
-                    'videotestsrc ! decodebin',
-                    '! videoconvert ! video/x-raw,format=(string)BGR ! videoconvert',
-                    '! appsink'
-                ]
-
-        command = ' '.join(config)
-        self.video_pipe = Gst.parse_launch("fakesrc ! video/x-raw, format=BGR ,width=1920,height=1080,framerate=30/1 ! appsink")
+    def start_gst(self):
+        self.video_pipe = Gst.parse_launch(f"udpsrc do-timestamp=true port={self.port} ! application/x-rtp ! rtph264depay ! avdec_h264 discard-corrupted-frames=true ! videoconvert ! video/x-raw,format=BGR ! appsink name=appsink0 sync=false emit-signals=true")
         self.video_pipe.set_state(Gst.State.PLAYING)
         self.video_sink = self.video_pipe.get_by_name('appsink0')
 
@@ -92,15 +50,16 @@ class Video():
             TYPE: Description
         """
         buf = sample.get_buffer()
-        caps_structure = sample.get_caps().get_structure(0)
+        caps = sample.get_caps()
         array = np.ndarray(
             (
-                caps_structure.get_value('height'),
-                caps_structure.get_value('width'),
+                caps.get_structure(0).get_value('height'),
+                caps.get_structure(0).get_value('width'),
                 3
             ),
-            buffer=buf.extract_dup(0, buf.get_size()), dtype=np.uint8)
-        return array
+            buffer=buf.extract_dup(0, buf.get_size()), dtype=np.uint8
+        )
+        return cv2.cvtColor(array, cv2.COLOR_BGR2RGB)
 
     def frame(self):
         """ Get Frame
@@ -126,13 +85,7 @@ class Video():
         """ Get frame to update _new_frame
         """
 
-        self.start_gst(
-            [
-                self.video_source,
-                self.video_codec,
-                self.video_decode,
-                self.video_sink_conf
-            ])
+        self.start_gst()
 
         self.video_sink.connect('new-sample', self.callback)
 
@@ -145,7 +98,7 @@ class Video():
 if __name__ == '__main__':
     # Create the video object
     # Add port= if is necessary to use a different one
-    video = Video()
+    video = Video(5601)
 
     print('Initialising stream...')
     waited = 0
