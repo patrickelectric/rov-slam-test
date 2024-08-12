fc7d34464a70b34453c30ccfeca03ab3219137c0
Joao Mario Lago <joao.mario.lago@hotmail.com>
2024-08-12 14:26:06 -0700

    Fix tags images in world position

--- navigation/world.py
+++ navigation/world.py
@@ -4,6 +4,7 @@ import rerun as rr
 from utils.vector import Vec3
 from typing import Dict
 import cv2
+import numpy as np
 
 
 class TagData(BaseModel):
@@ -35,29 +36,28 @@ class World:
         # Initialize known tags (code remains the same)
         for tag in self.tags:
             aruco_image = self.gen_aruco_img(tag.id, tag.size_m)
-            aruco_image = cv2.flip(aruco_image, 0)
-            rr.log(
-                f"world/tag_{tag.id}/image",
-                rr.Pinhole(
-                    focal_length=60,
-                    width=aruco_image.shape[1],
-                    height=aruco_image.shape[0],
-                ),
-            )
-            rr.log(
-                f"world/tag_{tag.id}/image",
-                rr.Image(aruco_image)
-            )
+            aruco_image = cv2.cvtColor(aruco_image, cv2.COLOR_GRAY2RGB)
+            tensor_data = rr.TensorData(array=aruco_image.astype(np.uint8))
             rr.log(
                 f"world/tag_{tag.id}",
-                rr.Transform3D(
-                    translation=[tag.position.x + tag.size_m / 2.0, tag.position.y - tag.size_m / 2.0, tag.position.z]
+                rr.Mesh3D(
+                    vertex_positions=[
+                        [tag.position.x, tag.position.y - tag.size_m, 0.0],
+                        [tag.position.x + tag.size_m, tag.position.y - tag.size_m, 0.0],
+                        [tag.position.x + tag.size_m, tag.position.y, 0.0],
+                        [tag.position.x, tag.position.y, 0.0],
+                    ],
+                    vertex_normals=[[0.0, 0.0, 1.0]] * 4,
+                    vertex_colors=[[255, 255, 255]] * 4,
+                    triangle_indices=[0, 1, 2, 0, 2, 3],
+                    vertex_texcoords=[[0.0, 1.0], [1.0, 1.0], [1.0, 0.0], [0.0, 0.0]],
+                    albedo_texture=tensor_data,
                 ),
             )
 
     def gen_aruco_img(self, tag_id: int, size: float) -> None:
         aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_ARUCO_ORIGINAL)
-        tag_size_px = int(size * 3000)
+        tag_size_px = int(size * 5000)
         return cv2.aruco.generateImageMarker(aruco_dict, tag_id, tag_size_px)
 
     def get_abs_tag(self, tag_id: int) -> TagData | None:
